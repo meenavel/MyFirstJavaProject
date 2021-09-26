f3e3bd4e3ecb40ef111173120f64971bde4d46eb
meenavel <Meena.velumayil@outlook.com>
2021-09-26 23:46:59 +1000

    My first psvm

--- src/mv/java/learning/basics/MyFirstMainClass.java
+++ src/mv/java/learning/basics/MyFirstMainClass.java
@@ -6,4 +6,9 @@ My First Main class
 - Create my first declaration and Initialization
  */
 public class MyFirstMainClass {
+    // My first static main method
+    public static void main(String[] args) {
+        // My first sout 
+        System.out.println("My First print");
+    }
 }
