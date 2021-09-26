040fde446bfb4cf5752cd0e5d67c97f7c37cc1c4
meenavel <Meena.velumayil@outlook.com>
2021-09-26 23:41:50 +1000

    My first change

--- src/mv/java/learning/basics/MyFirstMainClass.java
+++ src/mv/java/learning/basics/MyFirstMainClass.java
@@ -1,4 +1,9 @@
 package mv.java.learning.basics;
-
+/*
+My First Main class
+- Create my first main method
+- Create my first import
+- Create my first declaration and Initialization
+ */
 public class MyFirstMainClass {
 }
